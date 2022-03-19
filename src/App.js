88cc7850a9b437351a8421fb2c9c7d160848fd0f
raghava20 <raghavaanand22@gmail.com>
2022-03-19 19:29:01 +0530

    done initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
 import './App.css';
+import { Provider } from "react-redux"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import store from "./redux/store"
+
+import jwtDecode from "jwt-decode"
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import PageNotFound from './pages/404 page/PageNotFound';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
+import Dashboard from './pages/dashboard page/Dashboard';
+import { getLoggedInUserData, logout } from './redux/actions/authActions';
+import { SET_AUTHENTICATED } from './redux/types';
+import PostsPage from './pages/posts page/PostsPage';
+import CreatePostPage from './pages/create post/CreatePostPage';
+import PostPage from './pages/post page/PostPage';
+import ProfilesPage from './pages/profiles page/ProfilesPage';
+import Appbar from './components/Appbar';
+import API_URL from './utils/API_URL';
+import ApplicantsPage from './pages/applicants page/ApplicantsPage';
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      // Purple and green play nicely together.
+      main: "#303f9f",
+    },
+    secondary: {
+      // This is green.A700 as hex.
+      main: '#e8eaf6',
+    },
+  },
+});
+
+const token = localStorage.getItem("token")
+if (token) {
+  const decodeToken = jwtDecode(token)
+  if (decodeToken.exp * 1000 <= Date.now()) {
+    store.dispatch(logout())
+    window.location.href = "/login"
+  }
+  else {
+    store.dispatch({ type: SET_AUTHENTICATED })
+    API_URL.defaults.headers.common["x-auth-token"] = token;
+    store.dispatch(getLoggedInUserData())
+  }
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Appbar />
+
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+
+            <Route path="post">
+              <Route path="create" element={<CreatePostPage />} />
+              <Route path="me" element={<PostsPage />} />
+              <Route path=":id" element={<PostPage />} />
+            </Route>
+
+            <Route path="profile" >
+              <Route path="applicants" element={<ApplicantsPage />} />
+            </Route>
+
+            <Route path="applied-jobs" element={<ProfilesPage />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Appbar.js
@@ -0,0 +1,225 @@
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import { Box, Button, Container, IconButton, Menu, MenuItem, Toolbar, Tooltip, Typography } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useDispatch, useSelector } from "react-redux"
+import { logout } from '../redux/actions/authActions';
+import PersonIcon from '@mui/icons-material/Person';
+
+export default function Appbar() {
+
+    const dispatch = useDispatch();
+    const { authenticated, user: { account } } = useSelector(state => state.auth)
+
+    const candidate = ['Jobs'];
+    const candidateProfile = ['Applied Jobs', 'Logout'];
+
+    const recruiter = ['Jobs'];
+    const recruiterProfile = ['Post Job', 'My posts', 'Logout']
+
+    const [user, setUser] = useState("")
+    const [userProfile, setUserProfile] = useState("")
+    const [anchorElNav, setAnchorElNav] = useState(null);
+    const [anchorElUser, setAnchorElUser] = useState(null);
+
+    useEffect(() => {
+        getDataFromDb()
+    }, [])
+
+    const getDataFromDb = async () => {
+        if (await account) {
+            setUser(account.role)
+            setUserProfile(account.role)
+        }
+    }
+
+    let navigate = useNavigate()
+
+    const endpointHandler = (endpoint) => {
+        console.log(endpoint)
+        if (endpoint.includes("Applied Jobs")) return navigate("/applied-jobs")
+        else if (endpoint.includes("Jobs")) return navigate("/")
+        else if (endpoint.includes("Post Job")) return navigate("/post/create")
+        else if (endpoint.includes("My posts")) return navigate("/post/me")
+        else if (endpoint.includes("Logout")) return dispatch(logout(navigate))
+    }
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    return (
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters sx={{ display: "flex", justifyContent: "space-between" }}>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="div"
+                        sx={{ mr: 2, display: { xs: 'none', md: 'flex' }, cursor: 'pointer' }}
+                        onClick={() => navigate("/")}
+                    >
+                        JOB FINDER
+                    </Typography>
+
+                    {authenticated ?
+                        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                            <IconButton
+                                size="large"
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleOpenNavMenu}
+                                color="inherit"
+                            >
+                                <MenuIcon />
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorElNav}
+                                anchorOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'left',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'left',
+                                }}
+                                open={Boolean(anchorElNav)}
+                                onClose={handleCloseNavMenu}
+                                sx={{
+                                    display: { xs: 'block', md: 'none' },
+                                }}
+                            >
+                                {(user === "recruiter" ? recruiter : candidate).map((page) => (
+                                    <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                        <Typography textAlign="center" onClick={() => endpointHandler(page)}>{page}</Typography>
+                                    </MenuItem>
+                                ))}
+                            </Menu>
+                        </Box>
+                        :
+                        ""
+                    }
+
+                    {/* big screen */}
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="div"
+                        sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+                    >
+                        JOB FINDER
+                    </Typography>
+                    {authenticated ?
+                        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                            {(user === "recruiter" ? recruiter : candidate).map((page) => (
+                                <Button
+                                    key={page}
+                                    onClick={() => {
+                                        handleCloseNavMenu()
+                                        endpointHandler(page)
+                                    }}
+                                    sx={{ my: 2, color: 'white', display: 'block' }}
+                                >
+                                    {page}
+                                </Button>
+                            ))}
+                        </Box>
+                        :
+                        ""
+                    }
+
+                    {/* authentication */}
+                    {authenticated ?
+                        <Box sx={{ flexGrow: 0 }}>
+                            <Tooltip title="Open settings">
+                                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                    <PersonIcon color="secondary" />
+                                </IconButton>
+                            </Tooltip>
+                            <Menu
+                                sx={{ mt: '45px' }}
+                                id="menu-appbar"
+                                anchorEl={anchorElUser}
+                                anchorOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                open={Boolean(anchorElUser)}
+                                onClose={handleCloseUserMenu}
+                            >
+                                {(userProfile === "recruiter" ? recruiterProfile : candidateProfile).map((setting) => (
+                                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                                        <Typography textAlign="center" onClick={() => endpointHandler(setting)}>{setting}</Typography>
+                                    </MenuItem>
+                                ))}
+                            </Menu>
+                        </Box>
+                        :
+                        <>
+                            <Box sx={{ flexGrow: 0, display: { xs: 'none', sm: 'flex' } }}>
+                                <Button color="inherit" onClick={() => navigate("/login")} size="small" sx={{ mx: 1 }}>Login</Button>
+                                <Button color="secondary" variant="outlined" onClick={() => navigate("/signup")} size="small">SignUp</Button>
+                            </Box>
+                            <Box sx={{ flexGrow: 1, display: { xs: 'flex', sm: 'none' }, justifyContent: "flex-end" }}>
+                                <IconButton
+                                    size="large"
+                                    aria-label="account of current user"
+                                    aria-controls="menu-appbar"
+                                    aria-haspopup="true"
+                                    onClick={handleOpenUserMenu}
+                                    color="inherit"
+                                >
+                                    <MenuIcon />
+                                </IconButton>
+                                <Menu
+                                    sx={{ mt: '45px' }}
+                                    id="menu-appbar"
+                                    anchorEl={anchorElUser}
+                                    anchorOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'right',
+                                    }}
+                                    keepMounted
+                                    transformOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'right',
+                                    }}
+                                    open={Boolean(anchorElUser)}
+                                    onClose={handleCloseUserMenu}
+                                >
+                                    <MenuItem onClick={handleCloseUserMenu}>
+                                        <Typography textAlign="center" onClick={() => navigate("/login")}>Login</Typography>
+                                    </MenuItem>
+                                    <MenuItem onClick={handleCloseUserMenu}>
+                                        <Typography textAlign="center" onClick={() => navigate("/signup")}>SignUp</Typography>
+                                    </MenuItem>
+
+                                </Menu>
+                            </Box>
+                        </>
+                    }
+                </Toolbar>
+            </Container>
+        </AppBar>
+    )
+}
