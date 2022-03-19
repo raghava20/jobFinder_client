7427257df66bb08a5a059e8e7889a429a9359350
raghava20 <raghavaanand22@gmail.com>
2022-03-19 20:01:28 +0530

    added backend url

--- src/utils/API_URL.js
+++ src/utils/API_URL.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API_URL = axios.create({
-    baseURL: "http://localhost:8000"
+    baseURL: "https://job-finder--app.herokuapp.com"
 })
 
 export default API_URL;
